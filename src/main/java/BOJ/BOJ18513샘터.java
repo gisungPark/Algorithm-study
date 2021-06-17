881b30829dec28ea410185090c54add7999bcbdc
gisung2215 <wjv2215@naver.com>
2021-06-17 22:02:15 +0900

    Solve: BOJ18513 샘터

--- src/main/java/BOJ/BOJ18513샘터.java
+++ src/main/java/BOJ/BOJ18513샘터.java
@@ -6,8 +6,8 @@ import java.util.*;
 public class BOJ18513샘터 {
 
     static int N, K, HALF;
-    static List<Integer> water, home;
-    static int[] visited;
+    static Queue<int[]> water;
+    static Set<Integer> set;
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,72 +16,41 @@ public class BOJ18513샘터 {
         K = Integer.parseInt(st.nextToken());
         HALF = 100000000;
 
-        visited = new int[HALF * 2 + 2];
-        water = new ArrayList<>();
-        home = new ArrayList<>();
+        set = new HashSet<>();
+        water = new LinkedList<>();
 
         // 1. 샘물 입력
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<N; i++){
             int pos = Integer.parseInt(st.nextToken());
-            visited[pos+HALF] = 1;
-            water.add(pos);
+            set.add(pos);
+            water.add(new int[]{pos, 0});
         }
 
-        Collections.sort(water);
+        long sum = 0;
 
-        int round = 1;
-        loop: while(K > 0){
-            for(int i=0; i<water.size(); i++){
-                int cur = water.get(i); // 샘물 좌표
-                int left = cur - round; // 왼쪽 좌표
-                int right = cur + round; // 오른쪽 좌표
+        loop: while(!water.isEmpty()){
+            int[] cur = water.poll();
+            int left = cur[0] - 1;
+            int right = cur[0] + 1;
+            // 왼쪽 비교
+            if(!set.contains(left)){
+                set.add(left);
+                water.add(new int[]{left, cur[1]+1});
+                sum += cur[1]+1;
+                if(--K <= 0) break loop;
 
-                if(left >= HALF * -1
-                            && visited[HALF+left] == 0){
-                    
-                    // 2. 샘물과의 최소값 구하기
-                    visited[HALF+left] = getMinDis(left, i);
-                    home.add(left);
-                    if(--K <= 0) break loop;
-                }
-                if(right <= HALF
-                            && visited[HALF+right] == 0){
-
-                    // 2. 샘물과의 최소값 구하기
-                    visited[HALF+right] = getMinDis(right, i);
-                    home.add(right);
-                    if(--K <= 0) break loop;
-                }
             }
-            round++;
-        }
-        int sum = 0;
-        for(int i=0; i < home.size(); i++){
-            sum += visited[HALF+home.get(i)];
+            
+            // 오른쪽 비교
+            if(!set.contains(right)){
+                set.add(right);
+                water.add(new int[]{right, cur[1]+1});
+                sum += cur[1]+1;
+                if(--K <= 0) break loop;
+            }
+
         }
         System.out.println(sum);
     }
-
-    /**
-     * pos 위치에서 샘물과의 최소값 계산
-     */
-    private static int getMinDis(int pos, int idx) {
-        int prev, cur, next;
-        prev = cur = next = water.get(idx);
-
-        if(idx - 1 >= 0 ) prev = water.get(idx-1);
-        if(idx + 1 < N) next = water.get(idx+1);
-        cur = water.get(idx);
-
-
-        return Math.min(dis(pos, prev),
-                Math.min(dis(pos, cur), dis(pos, next)));
-    }
-
-    public static int dis(int from, int to){
-        if(from > to) return from - to;
-        else return to - from;
-    }
-
 }
