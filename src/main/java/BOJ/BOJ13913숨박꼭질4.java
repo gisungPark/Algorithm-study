bc1c41c5e8e59de7cebd7ff6ed0bad098709b428
gisung2215 <wjv2215@naver.com>
2021-06-18 23:44:19 +0900

    Solve: BOJ13913 숨박꼭질4

--- src/main/java/BOJ/BOJ13913숨박꼭질4.java
+++ src/main/java/BOJ/BOJ13913숨박꼭질4.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class BOJ13913숨박꼭질4 {
 
     static int N, K;
+    static int[] parents;
     static Set<Integer> set;
     public static void main(String[] args) {
 
@@ -12,63 +13,70 @@ public class BOJ13913숨박꼭질4 {
         N = sc.nextInt();
         K = sc.nextInt();
 
+        parents = new int[100003];
         set = new HashSet<>();
+        parents[N] = N;
         set.add(N);
 
-        Queue<path> q = new LinkedList<>();
-        // 1. 출발점 초기화
-        q.add(new path(N, 0, new ArrayList()));
+        if(N == K){
+            System.out.println(0);
+            System.out.println(N);
+        }else {
+            Queue<int[]> q = new LinkedList<>();
+            // 1. 출발점 초기화
+            q.add(new int[]{N, 0});
 
-        while(!q.isEmpty()){
-            path cur = q.poll();
-            // 1. 걷기
-            if(move(q, cur, 0)) break;
-            if(move(q, cur, 1)) break;
-            if(move(q, cur, 2)) break;
+            while (!q.isEmpty()) {
+                int[] cur = q.poll();
+                // 1. 걷기
+                if (move(q, cur, 0)) break;
+                if (move(q, cur, 1)) break;
+                if (move(q, cur, 2)) break;
+            }
         }
     }
 
-    private static boolean move(Queue<path> q, path cur, int flag) {
+    private static boolean move(Queue<int[]> q, int[] cur, int flag) {
 
         int xx = 0;
         switch (flag){
             case 0:
-                xx = cur.x + 1;
+                xx = cur[0] + 1;
                 break;
             case 1:
-                xx = cur.x -1;
+                xx = cur[0] -1;
                 break;
             case 2:
-                xx = cur.x * 2;
+                xx = cur[0] * 2;
                 break;
         }
         if(xx < 0 || xx > 100000 || set.contains(xx)) return false;
+        parents[xx] = cur[0];
+        set.add(xx);
 
         // 목적지 도착!!
         if(xx == K) {
-            System.out.println(cur.dis + 1);
-            for(int i=0; i<cur.list.size(); i++){
-                System.out.print(cur.list.get(i) + " ");
+            int child = xx;
+            System.out.println(cur[1] + 1);
+            List<Integer> path = new ArrayList<>();
+            path.add(child);
+            while(parents[child] != N){
+                child = parents[child];
+                path.add(child);
             }
-            System.out.println(xx);
+
+            System.out.print(N+" ");
+            for(int i=path.size()-1; i>=0; i--){
+                System.out.print(path.get(i) + " ");
+            }
+
             return true;
         }
 
-        q.add(new path(xx, cur.dis+1, cur.list));
+
+
+        q.add(new int[]{xx, cur[1] + 1});
         return false;
     }
 
-
-    public static class path{
-        int x;
-        int dis;
-        List<Integer> list = new ArrayList<>();
-
-        public path(int x, int dis, List list){
-            this.x = x;
-            this.dis = dis;
-            this.list.addAll(list);
-            this.list.add(x);
-        }
-    }
 }
