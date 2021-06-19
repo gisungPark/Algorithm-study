990cfa88dec15c524a8b7621716517193083fe7c
gisung2215 <wjv2215@naver.com>
2021-06-19 23:14:47 +0900

    Solve: BOJ9019 DSLR 코드 개선

--- src/main/java/BOJ/BOJ9019DSLR.java
+++ src/main/java/BOJ/BOJ9019DSLR.java
@@ -8,6 +8,7 @@ public class BOJ9019DSLR {
     static obj[] visited;
     static int A, B;
     static char[] command = {'D', 'S', 'L', 'R'};
+
     public static class obj{
         int parents;
         char op;
@@ -20,16 +21,15 @@ public class BOJ9019DSLR {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+
         T = sc.nextInt();
-        for(int tc=0; tc<T; tc++){
+        for (int tc = 0; tc < T; tc++) {
             visited = new obj[10000];
             A = sc.nextInt();
             B = sc.nextInt();
-
             process(A, B);
         }
     }
-
 
     private static void process(int from, int to) {
 
@@ -38,20 +38,19 @@ public class BOJ9019DSLR {
         visited[from] = new obj(from, ' ');
 
         loop: while(!q.isEmpty()){
-            int cur = q.poll();                 // 현재위치
-            char op =' ';
-            for(int i=0; i<4; i++){             // 다음위치
+            int cur = q.poll();                     // 현재위치
+            for (int i = 0; i < 4; i++) {             // 다음위치
                 int next = go(cur, i);
-                if(visited[next] != null) continue;
+                if (visited[next] != null) continue;
                 // 방문처리!!
                 visited[next] = new obj(cur, command[i]);
                 q.add(next);
 
-                if(next == to){
+                if (next == to) {
                     List<Character> list = new ArrayList<>();
                     int parents = next;
                     // 부모가 출발점이 아닐때까지,
-                    while(parents != from){
+                    while (parents != from) {
                         list.add(visited[parents].op);
                         parents = visited[parents].parents;
                     }
@@ -61,54 +60,15 @@ public class BOJ9019DSLR {
                     System.out.println();
                     break loop;
                 }
-
             }
-
         }
     }
 
     private static int go(int pos, int flag) {
-        switch (flag){
-            case 0:
-                pos *= 2;
-                if(pos > 9999) pos %= 10000;
-                break;
-            case 1:
-                if(pos == 0) pos = 9999;
-                else pos--;
-                break;
-            case 2:
-                pos = pushLeft(convertToString(pos));
-                break;
-            case 3:
-                pos = pushRight(convertToString(pos));
-                break;
-        }
-        return pos;
+        if (flag == 0) return (pos * 2) % 10000;
+        if (flag == 1) return pos == 0 ? 9999 : (pos - 1);
+        if (flag == 2) return (pos % 1000) * 10 + (pos / 1000);
+        if (flag == 3) return (pos % 10) * 1000 + (pos / 10);
+        return -1;
     }
-
-    private static int pushRight(String cur) {
-        int d1 = cur.charAt(0) - '0';
-        int d2 = cur.charAt(1) - '0';
-        int d3 = cur.charAt(2) - '0';
-        int d4 = cur.charAt(3) - '0';
-        return (((d4 * 10) + d1) * 10 + d2) * 10 + d3;
-    }
-
-    private static int pushLeft(String cur) {
-        int d1 = cur.charAt(0) - '0';
-        int d2 = cur.charAt(1) - '0';
-        int d3 = cur.charAt(2) - '0';
-        int d4 = cur.charAt(3) - '0';
-        return (((d2 * 10) + d3) * 10 + d4) * 10 + d1;
-    }
-
-    private static String convertToString(int N){
-        if(N>999) return String.valueOf(N);
-        else if(N>99) return "0"+N;
-        else if(N>9) return "00"+N;
-        else return "000"+N;
-    }
-
-
 }
